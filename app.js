85f999e588e1fe9c035156cdab9ae73b090f9e46
Thitiwas <champtnpc@gmail.com>
2016-08-20 15:31:02 +0700

    hello vuejs

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-new Vue({
+var app = new Vue({
   el: '#app',
-  data:{
-    msg : "hello",
-  }
+  data: {
+    msg: 'Pokemon Ranger',
+    list:[]
+  },
+
+  methods: {
+    getApi: function () {
+      var url = 'http://www.pokesnipers.com/api/v1/pokemon.json'
+      var that =this
+      this.$http.get(url).then(function (res) {
+        this.list = res.data.results
+        console.log(that.list);
+      })
+    }
+  },
+    ready:function(){
+        this.getApi()
+          var that1 = this
+      setInterval(function(){
+
+        that1.getApi()
+      },2000)
+    }
 })
+console.log(app)
